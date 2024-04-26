5e8384e4282cff31f5d13957ff3958c31360d01d
Jan Werner <jan.werner@guetersloh.de>
2024-04-26 08:50:54 +0200

    change shape from berlin to guetersloh

--- harvester/prepare.py
+++ harvester/prepare.py
@@ -2,15 +2,15 @@
 import geopandas
 from shapely.ops import cascaded_union
 
-berlin = geopandas.read_file("./assets/Berlin.shp")
-berlin = berlin.to_crs("epsg:3857")
-berlin_boundary = geopandas.GeoDataFrame(geopandas.GeoSeries(cascaded_union(berlin['geometry'])))
-berlin_boundary = berlin_boundary.rename(columns={0:'geometry'}).set_geometry('geometry')
+guetersloh = geopandas.read_file("./assets/Guetersloh.shp")
+guetersloh = guetersloh.to_crs("epsg:3857")
+guetersloh_boundary = geopandas.GeoDataFrame(geopandas.GeoSeries(cascaded_union(guetersloh['geometry'])))
+guetersloh_boundary = guetersloh_boundary.rename(columns={0:'geometry'}).set_geometry('geometry')
 
-berlin_buffer = berlin_boundary.buffer(2000)
-berlin_buffer = berlin_buffer.simplify(1000)
+guetersloh_buffer = guetersloh_boundary.buffer(2000)
+guetersloh_buffer = guetersloh_buffer.simplify(1000)
 
-berlin_buffer = geopandas.GeoDataFrame(berlin_buffer)
-berlin_buffer = berlin_buffer.rename(columns={0:'geometry'}).set_geometry('geometry')
-berlin_buffer.crs = "epsg:3857"
-berlin_buffer.to_file("./assets/buffer.shp")
+guetersloh_buffer = geopandas.GeoDataFrame(guetersloh_buffer)
+guetersloh_buffer = guetersloh_buffer.rename(columns={0:'geometry'}).set_geometry('geometry')
+guetersloh_buffer.crs = "epsg:3857"
+guetersloh_buffer.to_file("./assets/buffer.shp")
